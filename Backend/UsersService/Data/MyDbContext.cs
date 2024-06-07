becef6ac32c9a8dcb59f58d0f4139fcd96e5df85
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2024-06-07 14:21:50 +0300

    USERS Service
    scaffolded user db
    added models
    added basic controller for the user service

---
+++ Backend/UsersService/Data/MyDbContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using UsersService.Models;
+
+namespace UsersService.Data;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Follow> Follows { get; set; }
+
+    public virtual DbSet<History> Histories { get; set; }
+
+    public virtual DbSet<Like> Likes { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Follow>(entity =>
+        {
+            entity.HasKey(e => new { e.FollowerId, e.FollowedId }).HasName("PK__Follows__F7A5FC9F118714B3");
+
+            entity.Property(e => e.FollowedAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Followed).WithMany(p => p.FollowFolloweds)
+                .HasForeignKey(d => d.FollowedId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Follows__Followe__60A75C0F");
+
+            entity.HasOne(d => d.Follower).WithMany(p => p.FollowFollowers)
+                .HasForeignKey(d => d.FollowerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Follows__Followe__5FB337D6");
+        });
+
+        modelBuilder.Entity<History>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.AudioId }).HasName("PK__History__0DA06509F89C8D66");
+
+            entity.ToTable("History");
+
+            entity.Property(e => e.ListenedAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Histories)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__History__UserId__6754599E");
+        });
+
+        modelBuilder.Entity<Like>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.AudioId }).HasName("PK__Likes__0DA06509E8289199");
+
+            entity.Property(e => e.LikedAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Likes)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Likes__UserId__6477ECF3");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC073ED07557");
+
+            entity.Property(e => e.CreatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+            entity.Property(e => e.UserSecret)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Backend/UsersService/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsersService.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
+
+    public string UserSecret { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<Follow> FollowFolloweds { get; set; } = new List<Follow>();
+
+    public virtual ICollection<Follow> FollowFollowers { get; set; } = new List<Follow>();
+
+    public virtual ICollection<History> Histories { get; set; } = new List<History>();
+
+    public virtual ICollection<Like> Likes { get; set; } = new List<Like>();
+}
