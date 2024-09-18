7eafb1f0200e35e0f36981baec20c58a40aa35fd
ahmadbsese <ahmad.bsese@e.braude.ac.il>
2024-09-18 21:23:25 +0300

    explorePage- Saved - Profile screens

--- Frontend/audiowave6/lib/view/Home.dart
+++ Frontend/audiowave6/lib/view/Home.dart
@@ -1,5 +1,6 @@
-import 'package:audiowave6/view/Helpers/GeneralHelpers/AppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:audiowave6/view/Helpers/GeneralHelpers/AppBar.dart';
+import 'package:audiowave6/view/Helpers/GeneralHelpers/Blocks.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,31 +10,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // code
-  // Variables
   List AudiNames = ['test1', 'test2', 'test3'];
   List CreatorNames = ['test1', 'test2', 'test3'];
   List descriptions = ['test1', 'test2', 'test3'];
   List Durations = ['test1', 'test2', 'test3'];
   List Listening = [10, 20, 30];
   List UploadDate = ['test1', 'test2', 'test3'];
-  List Sizes = [10, 20, 30];
+  List Sizes = [10, 20, 30]; // Sizes for the container
   List Types = ['test1', 'test2', 'test3'];
-  List ids = [1, 2, 3];
-  List<int> UploaderIds = [11, 22, 33];
+  List imagePaths = ['assets/BG1.jpg', 'assets/BG2.jpg', 'assets/BG3.jpg']; // Images for the audio files
   int len = 0;
   bool show = false;
-  // Variables
-  // Functions
-  // Functions
-  // Widgets
-  // Widgets
-  // code
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     len = AudiNames.length;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,67 +35,33 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         children: [
           GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1,),
-              itemCount: len,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(padding: EdgeInsets.fromLTRB(20, 15, 15, 10),
-                  child: Container(color: Colors.white,
-                    child: InkWell(
-                      onTap: (){
-
-                      },
-                      onDoubleTap: (){
-                        setState(() {
-                          show = !show;
-                        });
-                      },
-                      onLongPress: (){
-
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(),
-                          image: DecorationImage(image: AssetImage('assets/BG1.jpg'),
-                          fit: BoxFit.fill),
-                        ),
-                        child: Column (
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                              child: Text(AudiNames[index], style: TextStyle(fontSize: 33),
-                                textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-                            show ? Column(
-                              children: [
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Text('Creator: ' + CreatorNames[index], style: TextStyle(fontSize: 33),
-                                    textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Text('Description: ' + descriptions[index], style: TextStyle(fontSize: 33),
-                                    textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Text('Audio Duration: ' + Durations[index], style: TextStyle(fontSize: 33),
-                                    textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Text('Listened: ' + Listening[index].toString() + ' times', style: TextStyle(fontSize: 33),
-                                    textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Text('Uploaded on: ' + UploadDate[index], style: TextStyle(fontSize: 33),
-                                    textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Text('Audio File Size: ' + Sizes[index].toString(), style: TextStyle(fontSize: 33),
-                                    textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  child: Text('Audio File Type: ' + Types[index], style: TextStyle(fontSize: 33),
-                                    textDirection: TextDirection.rtl, textAlign: TextAlign.center,),),
-
-                              ],
-                            ) : SizedBox(),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),);
-              }
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+            ),
+            itemCount: len,
+            itemBuilder: (BuildContext context, int index) {
+              return AudioBlockHelper.buildAudioBlock(
+                audioName: AudiNames[index],
+                creatorName: CreatorNames[index],
+                description: descriptions[index],
+                duration: Durations[index],
+                listening: Listening[index],
+                uploadDate: UploadDate[index],
+                size: Sizes[index],
+                type: Types[index],
+                showDetails: show,
+                imagePath: imagePaths[index], // Passing the image path
+                containerHeight: Sizes[index].toDouble(), // Passing the size as height
+                onTap: () {
+                  // handle tap event
+                },
+                onDoubleTap: () {
+                  setState(() {
+                    show = !show;
+                  });
+                },
+              );
+            },
           ),
         ],
       ),
